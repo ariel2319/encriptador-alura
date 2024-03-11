c48a5ae3f18ce81ac05501439ca0fbbe5baa5031
ariel2319 <maldonadoaa.91@gmail.com>
2024-03-11 01:46:45 -0300

    js ready, responsive iniciando

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 
+let textArea = document.getElementById("texto");
+textArea.addEventListener("input", function (event) {
+  let texto = event.target.value;
+  let textoMinusculas = texto.toLowerCase().replace(/[áéíóúÁÉÍÓÚüÜñÑ1234567890¨´]/g, "");
 
+  if (texto !== textoMinusculas) {
+    event.target.value = textoMinusculas;
+  }
+});
 
 function encriptar() {
   let texto = document.getElementById("texto").value;
   let resultado = document.getElementById("resultado");
+
   resultado.value = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
 }
 
 function desencriptar() {
   let texto = document.getElementById("texto").value;
   let resultado = document.getElementById("resultado");
+
   resultado.value = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
 }
 
@@ -18,6 +28,7 @@ function copiar() {
   resultado.select();
   document.execCommand("copy");
 }
+
 function pegarTexto() {
   navigator.clipboard.readText().then(text => {
     document.getElementById("texto").value = text;
