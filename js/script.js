0549f018a4ce18aaf048cc943f6fd5a3911e0563
ariel2319 <maldonadoaa.91@gmail.com>
2024-03-10 22:00:12 -0300

    reset button ready

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
+
+
+
 function encriptar() {
-  var texto = document.getElementById("texto").value;
-  var resultado = document.getElementById("resultado");
+  let texto = document.getElementById("texto").value;
+  let resultado = document.getElementById("resultado");
   resultado.value = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
 }
 
 function desencriptar() {
-  var texto = document.getElementById("texto").value;
-  var resultado = document.getElementById("resultado");
+  let texto = document.getElementById("texto").value;
+  let resultado = document.getElementById("resultado");
   resultado.value = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
 }
 
 function copiar() {
-  var resultado = document.getElementById("resultado");
+  let resultado = document.getElementById("resultado");
   resultado.select();
   document.execCommand("copy");
 }
@@ -21,4 +24,11 @@ function pegarTexto() {
   }).catch(err => {
     console.error('Error al pegar texto: ', err);
   });
+}
+
+function limpiar() {
+  document.getElementById("texto").value = " ";
+  /*  let textAreaTexto = document.getElementById("texto");
+   textAreaTexto.placeholder = "newww"; */
+  document.getElementById("resultado").value = " ";
 }
